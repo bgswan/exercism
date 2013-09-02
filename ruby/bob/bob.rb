93c78b7cfd8262bfa9bd001fdaf77a0eb5895ff9
Brian Swan <bgswan@gmail.com>
2013-09-02 22:38:04 +0100

    Initial commit

---
+++ ruby/bob/bob.rb
@@ -0,0 +1,34 @@
+class Dialogue
+  def initialize(message)
+    @message = message.strip
+  end
+
+  def shouted?
+    !empty? && @message == @message.upcase
+  end
+
+  def question?
+    @message.end_with? '?'
+  end
+
+  def empty?
+    @message.empty?
+  end
+end
+
+class Bob
+
+  def hey(message)
+    dialogue = Dialogue.new(message)
+    case
+    when dialogue.shouted?
+      'Woah, chill out!'
+    when dialogue.question?
+      'Sure.'
+    when dialogue.empty?
+      'Fine. Be that way!'
+    else
+      'Whatever.'
+    end
+  end
+end
